ed2566d840cc8b915eb5a4bb075e6a8ab5d32501
Tomasino Léo <leo.tomasino@gmail.com>
2022-11-16 16:06:24 +0100

    logger improved ++

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -10,6 +10,11 @@ public class LoggerFactory
         //return new ContextualLogger(name, new ConsoleLogger());
         //return new FileLogger(name);
         //return new ContextualLogger(name, new FileLogger(name));
-        return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FileLogger(name)));
+        Predicate<String> condition = msg -> msg.contains("simulation");
+
+        //return new FilteredLogger(new ContextualLogger(name, new ConsoleLogger()), condition);
+        return new ContextualLogger(name, new CompositeLogger(new ConsoleLogger(), new FilteredLogger(new FileLogger(name), condition)));
+
+        //return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FileLogger(name)));
     }
 }
